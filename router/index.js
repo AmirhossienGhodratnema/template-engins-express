00962fb89d17c5f94e1076c5edc5905c4b2d3631
amirhossein <amirhosseinghodratnema@gmail.com>
2023-02-24 01:19:11 +0330

    install pug template engin and configurations => 2

--- router/index.js
+++ router/index.js
@@ -7,9 +7,7 @@ const router = express.Router();
 // Main route in express.
 router.get('/', (req, res, next) => {
     try {
-        console.log(req.body)
-        if(req.body.name == 'Amirhossein') throw {statusCode : 400 , message : 'test test test'}
-        return res.json('ok...');     
+        return res.json('Main route is ok...');    
     } catch (error) {
         next(error);
     };
